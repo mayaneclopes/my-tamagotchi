2894d4b8f977f45bcede2c5174fd657ab2623775
Mayane Lopes <mayaneclopes@gmail.com>
2024-08-18 20:16:15 -0300

    Implementação dos botões e status

--- app/(tabs)/tamagotchi.tsx
+++ app/(tabs)/tamagotchi.tsx
@@ -1,18 +1,23 @@
 import { useEffect, useState } from "react";
 import { StyleSheet, View, Text, Button } from "react-native";
-import Svg, { Circle, Line } from 'react-native-svg';
+import Svg, { Circle, Line, Path } from 'react-native-svg';
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: "#FFFF",
+        backgroundColor: '#e3eeff',
     },
     text: {
         marginVertical: 5,
         fontSize: 18,
         color: "#000"
+    },
+    statusText: {
+        marginVertical: 10,
+        fontSize: 20,
+        fontWeight: "bold",
     },
 });
 
@@ -24,26 +29,80 @@ export default function Status() {
 
     useEffect(() => {
         const timer = setInterval(() => {
-            setHunger(prev => Math.max(0, prev - 5));
-            setHappy(prev => Math.max(0, prev - 5));
-            setSleep(prev => Math.max(0, prev - 10));
+            setHunger(prev => Math.max(0, Math.min(100, prev - 5)));
+            setHappy(prev => Math.max(0, Math.min(100, prev - 5)));
+            setSleep(prev => Math.max(0, Math.min(100, prev - 5)));
         }, 2000);
 
         return () => clearInterval(timer);
     }, []);
 
-    const face = (value: number) => {
+    const getFaceExpression = (value: number) => {
+        if (value >= 70) {
+            return 'smile';
+        } else if (value >= 40) {
+            return 'neutral';
+        } else {
+            return 'sad';
+        }
+    };
 
+    const calculateStatus = () => {
+        const statusTotal = hunger + sleep + happy;
+        if (statusTotal === 0) return "Morto x-x";
+        if (statusTotal <= 50) return "Crítico o_o";
+        if (statusTotal <= 100) return "Muito triste :'(";
+        if (statusTotal <= 150) return "Triste :(";
+        if (statusTotal <= 200) return "Ok :I";
+        if (statusTotal > 200) return "Bem! :)";
     }
+
+    const renderFace = (expression: string) => {
+        switch (expression) {
+            case "smile":
+                return (
+                    <>
+                        <Circle cx='35' cy='40' r='5' fill='black' />
+                        <Circle cx='65' cy="40" r="5" fill='black' />
+                        <Path d="M35 60 Q50 75, 65 60"
+                            stroke='black'
+                            strokeWidth='2.5'
+                            fill='none' />
+                    </>
+                );
+            case 'neutral':
+                return (
+                    <>
+                        <Circle cx='35' cy='40' r='5' fill='black' />
+                        <Circle cx='65' cy="40" r="5" fill='black' />
+                        <Line x1='35' y1='60'
+                            x2='65' y2='60'
+                            stroke="black" strokeWidth='2.5'
+                        />
+                    </>
+                );
+            case 'sad':
+                return (
+                    <>
+                        <Circle cx='35' cy='40' r='5' fill='black' />
+                        <Circle cx='65' cy="40" r="5" fill='black' />
+                        <Path d="M35 60 Q50 45, 65 60"
+                            stroke='black'
+                            strokeWidth='2.5'
+                            fill='none' />
+                    </>
+                )
+        }
+    }
+
+    const faceExpression = getFaceExpression(happy);
 
     return (
         <View style={styles.container}>
             <Svg height='100' width='100' viewBox="0 0 100 100">
                 <Circle cx="50" cy='50' r='45' stroke='black'
                     strokeWidth='2.5' fill='yellow' />
-                <Circle cx='35' cy='40' r='5' fill='black' />
-                <Circle cx='65' cy='40' r='5' fill='black' />
-                <Line x1='35' y1='65' x2='65' y2='65' stroke="black" strokeWidth='2.5' />
+                {renderFace(faceExpression)}
             </Svg>
 
             <Text style={styles.text}>
@@ -56,14 +115,20 @@ export default function Status() {
                 Felicidade: {happy}
             </Text>
 
+            <Text style={styles.statusText}>Status: {calculateStatus()}</Text>
+
             <Button title="Alimentar" onPress={() =>
-                setHunger(Math.max(0, hunger + 10))
+                setHunger(
+                    prev => Math.max(0, Math.min(100, prev + 10)))
             } />
             <Button title="Brincar" onPress={() =>
-                setHappy(Math.min(100, happy + 10))
+                setHappy(
+                    prev => Math.max(0, Math.min(
+                        100, prev + 10)))
             } />
             <Button title="Dormir" onPress={() =>
-                setSleep(Math.min(100, sleep + 20))
+                setSleep(
+                    prev => Math.max(0, Math.min(100, prev + 10)))
             } />
         </View>
     );
